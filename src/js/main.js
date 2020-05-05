36b7a25a2729a276daccad18ccc91d0c58a6f365
Izripov-Yusuf <izripov1998@gmail.com>
2020-05-05 21:30:52 +0300

    Сверстал и адаптировал всё на главной, кроме секции с слайдером до/после и секции с картой

--- src/js/main.js
+++ src/js/main.js
@@ -34,9 +34,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const benefitAfter = document.querySelector('.benefit-after'),
         benefitbefore = document.querySelector('.benefit-before'),
         beforeArrow = document.querySelector('.benefit-before-arrow');
-  beforeArrow.addEventListener('click', () => {
-    benefitbefore.style.transform = 'translateX(-120%)';
-    benefitAfter.classList.add('benefit-after-active');
+  if (beforeArrow) {
+    beforeArrow.addEventListener('click', () => {
+      benefitbefore.style.transform = 'translateX(-120%)';
+      benefitAfter.classList.add('benefit-after-active');
+    });
+  }
+
+  //Аккордеон
+  $(".faq-accordeon dl").on('click', function () {
+    if ($(this).find(".arrow-down").hasClass('arrow-down-active')) {
+      $(this).find(".arrow-down").removeClass('arrow-down-active');
+      $(this).find("dd").slideUp();
+    } else {
+      $(".faq-accordeon dd").not(this).slideUp();
+      $(".faq-accordeon").find(".arrow-down").removeClass('arrow-down-active');
+      $(this).find(".arrow-down").toggleClass('arrow-down-active');
+      $(this).find("dd").not(":visible").slideDown();
+    }
   });
 
   // hero слайдер на мобильной версии
@@ -58,17 +73,71 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // benefit слайдер на мобильной версии
-  const benefitSliderSection = document.querySelector('.benefit-section'),
-        benefitSliderId = document.getElementById('benefit-slider');
+  const benefitSliderSection = document.querySelector('.benefit-section');
 
-  if (window.innerWidth < 768) {
-    if (benefitSliderSection) {
-      new Swiper(benefitSliderId, {
-        direction: 'vertical',
-        slidesPerView: 1
+  if (benefitSliderSection) {
+    if (window.innerWidth < 768) {
+      $('#benefit-slider').slick({
+        infinite: false,
+        centerMode: true,
+        spaceBetween: 50,
+        arrows: false,
+        //centerPadding: '60px',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        verticalSwiping: true,
+        vertical: true,
       });
     }
   }
+
+  // Слайдер с отзывами
+  const reviewsSection = document.querySelector('.reviews-section');
+
+  if (reviewsSection) {
+    new Swiper('.reviews-slider', {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      autoplay: {
+        delay: 5000000,
+        disableOnInteraction: false,
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+        },
+        1023: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+
+  /* if (reviewsSection) {
+    $('.reviews-slider').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1199,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+      ]
+    });
+  } */
 });
 
 /* document.addEventListener('DOMContentLoaded', function () {
